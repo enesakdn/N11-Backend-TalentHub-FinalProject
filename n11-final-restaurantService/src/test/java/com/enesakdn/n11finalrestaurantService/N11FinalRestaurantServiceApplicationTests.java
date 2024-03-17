a9a18b04ec33a05939f4660ff9f81dba266b941b
enesakdn <enesakdogan948@gmail.com>
2024-03-17 04:15:12 +0300

    ExceptionFinished

---
+++ n11-final-restaurantService/src/test/java/com/enesakdn/n11finalrestaurantService/N11FinalRestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesakdn.n11finalrestaurantService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11FinalRestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
